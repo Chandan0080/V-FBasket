994d0c8c6bf2e5b0afd34a177b937db7a3d288b8
Dev Gupta <devgupta.2023cse@gmail.com>
2026-03-25 01:38:01 +0530

    changes for cart

--- src/main/java/com/V/FBasket/VnFBasket/dto/CartItemResponseDTO.java
+++ src/main/java/com/V/FBasket/VnFBasket/dto/CartItemResponseDTO.java
@@ -9,12 +9,17 @@ public class CartItemResponseDTO {
     private Double price;
     private Integer quantity;
     private Double totalPrice;
+    private String productImageUrl;
+    private int availableQuantity;
+    
 
-    public CartItemResponseDTO(Long productId, String productName, Double productPrice, Integer quantity, Double totalPrice) {
+    public CartItemResponseDTO(Long productId, String productName, Double productPrice, Integer quantity, Double totalPrice, String productImageUrl, int availableQuantity) {
         this.productId = productId;
         this.productName = productName;
         this.price = productPrice;
         this.quantity = quantity;
         this.totalPrice = totalPrice;
+        this.productImageUrl = productImageUrl;
+        this.availableQuantity = availableQuantity;
     }
 }
